1334f3e0fe02803235711e3fc87c59d72ada3f63
suin14 <sil0410@163.com>
2023-06-04 03:54:10 +0800

    初步完成商户注册、登录功能，商户用户注销及更改信息有缺陷

---
+++ src/main/java/com/example/smartairportsystem/mapper/merchantmapper.java
@@ -0,0 +1,17 @@
+package com.example.smartairportsystem.mapper;
+
+import com.example.smartairportsystem.entity.merchant;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
+public interface merchantmapper {
+    public void logupNewMerchant(merchant newmerchant);
+    public merchant getMerchantByEmail(String email);
+    public merchant getMerchantByID(Integer merchantid);
+    public merchant getMerchantByName(String realname);
+    // public void updateOldMerchant(person newmerchant);
+    // public void removeOldMerchant(Integer merchantid);
+    public List<merchant> listMerchantByMerchantid(Integer merchantid);
+}

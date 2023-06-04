7b6db0b2fe8eb11d5d6dc78c4e0c2aceec7393de
suin14 <sil0410@163.com>
2023-06-05 05:52:49 +0800

    初步开始工作人员模块

--- src/main/java/com/example/smartairportsystem/controller/staffcontroller.java
+++ src/main/java/com/example/smartairportsystem/controller/staffcontroller.java
@@ -8,4 +8,54 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/staff")
 public class staffcontroller {
+    @Resource
+    private staff staffService = new staffserviceimpl();
+    @Resource
+    private repairservice repairService = new repairserviceimpl();
+
+    //工作人员注册功能
+    @RequestMapping(value = "/logup",method = RequestMethod.POST)
+    public Map<String,Object> logupStaff(@RequestParam Map<String,String> rawmap){
+        Map<String,Object> map = new HashMap<>();
+
+        //表单取参
+        String realname = rawmap.get("realname");
+        String positionpost = rawmap.get("positionpost");
+        String email = rawmap.get("email");
+        String passwords = rawmap.get("passwords");
+        String repasswords = rawmap.get("repasswords");
+
+        try{
+            if(repasswords.equals(passwords)) {
+                //对用户设置的密码加盐加密后保存
+                Random root = new Random((new Random()).nextInt());
+                String salt = root.nextInt()+"";
+                tourist exist = touristService.getTouristByEmail(email);
+                if (exist != null) {
+                    map.put("success", false);
+                    map.put("message", "邮箱已被注册！");
+                } else {
+                    touristService.logupNewTourist(new tourist(0,email,passwords+salt,salt, "false"));
+                    map.put("success", true);
+                    map.put("message", "用户注册成功！");
+                }
+            }else{
+                map.put("success", false);
+                map.put("message", "确认密码不一致！");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            map.put("success",false);
+            map.put("message","用户注册失败！");
+        }
+        return map;
+    }
+
+    //工作人员登录
+    
+    //工作人员添加报修请求
+
+    //工作人员审核报修请求
+    
+    //工作人员审核商户入驻请求
 }
